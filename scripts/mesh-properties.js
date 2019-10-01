7e4b6aa438c24fe273a823a13326fe83ccc834d3
Stanisław Góra <stasgora@gmail.com>
2019-10-01 22:54:34 +0200

    Layer demo opacity sliders

--- scripts/mesh-properties.js
+++ scripts/mesh-properties.js
@@ -1,11 +1,25 @@
 $(() => {
-	let checkboxes = $('#mesh-properties').find('input[type=checkbox]');
+	let properties = $('#mesh-properties');
+	let checkboxes = properties.find('input[type=checkbox]');
 	checkboxes.each((index, element) => handleMeshLayerVisibilitySwitch(element));
 	checkboxes.change(event => {
 		handleMeshLayerVisibilitySwitch(event.target);
 	});
+	let sliders = properties.find('input[type=range]');
+	sliders.each((index, element) => handleMeshLayerOpacityChange(element));
+	sliders.on('input', event => {
+		handleMeshLayerOpacityChange(event.target);
+	});
 });
 
 function handleMeshLayerVisibilitySwitch(checkbox) {
-	$('#' + checkbox.dataset.svgLink).css('display', checkbox.checked ? 'block' : 'none');
+	getSvgByDataLink(checkbox).css('display', checkbox.checked ? 'block' : 'none');
+}
+
+function handleMeshLayerOpacityChange(slider) {
+	getSvgByDataLink(slider).css('opacity', $(slider).val());
+}
+
+function getSvgByDataLink(element) {
+	return $('#' + element.dataset.svgLink)
 }
